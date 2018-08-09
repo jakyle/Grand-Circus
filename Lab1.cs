2eb5542629a2e9e1a46eecbd742f9e4e185838b7
AEmenheiser <42221666+AEmenheiser@users.noreply.github.com>
2018-08-08 22:45:56 -0400

    Initial commit

---
+++ Lab1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+
+
+        public static void Main()
+        {
+
+            Console.Write("\n\n"); //begin user input
+            Console.Write("Check whether each cooresponding digit in two intigers sum to the same number or not:\n");
+            Console.Write("-------------------------------------------");
+            Console.Write("\n\n");
+            Console.Write("Input 1st number then hit enter: ");
+            string int1 = (Console.ReadLine());//user input 1
+
+
+            Console.Write("Input 2nd number: ");
+            string int2 = (Console.ReadLine());//user input 2
+
+            int[] numbers = new int[int1.ToString().Length]; //changing user inputs to strings for array
+            int[] numbers2 = new int[int2.ToString().Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(int1.Substring(i, 1));//populating arrays
+                numbers2[i] = int.Parse(int2.Substring(i, 1));
+            }
+
+
+            int[] numbers3 = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers3[i] = (numbers[i] + numbers2[i]);
+            }
+
+            int sum1 = numbers.Aggregate(0, (acc, x) => acc + x);
+            int sum2 = numbers2.Aggregate(0, (acc, x) => acc + x);
+            if
+                (sum1 == sum2)
+                Console.WriteLine("true");
+            else Console.WriteLine("false");
+
+            Console.ReadKey();
+        }
+                  
+    }
+    
+
+    }
+
+
+
+
+
+
+
+
+           
+
+
+
+
+        
+
+   
+
---
+++ Lab2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1;
+            DateTime date2;
+
+            Console.WriteLine("Enter the first date in dd/mm/yyyy format");
+            date1 = Convert.ToDateTime(Console.ReadLine()).Date;
+
+            Console.WriteLine("Enter the second date in dd/mm/yyyy format");
+            date2 = Convert.ToDateTime(Console.ReadLine()).Date;
+
+            TimeSpan difference = date2.Subtract(date1);
+
+            Console.WriteLine("Difference in Days---->" + difference.Days);
+            Console.WriteLine("Difference in Hours---->" + (difference.Days * 24));
+            Console.WriteLine("Difference in Minutes---->" + ((difference.Days * 24) * 60));
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
